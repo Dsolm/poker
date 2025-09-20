1a46a1a979ef189c39e60d547c0994d1047adac0
yiiingye <qtyingye@gmail.com>
2025-09-20 11:49:59 +0200

    Added Card component

---
+++ src/Card.svelte
@@ -0,0 +1,14 @@
+<script>
+    let {rank, type, color} = $props();
+
+</script>
+
+<img src={`./images/card_${rank}_${type}_${color}.png`} alt="card_${rank}_${type}_${color}">
+<style>
+    img{
+        height: 15%;
+        width: 10%;
+        border-color: aliceblue;
+    }
+</style>
+
